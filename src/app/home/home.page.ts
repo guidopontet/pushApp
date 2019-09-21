15478f5929bd23c9018af50a4cf49cb164a24ca5
Guido Pontet <guidopontet@gmail.com>
2019-09-21 18:43:43 -0300

    Save messages to storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PushService } from '../services/push.service';
+import { OSNotificationPayload } from '@ionic-native/onesignal/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  messages: OSNotificationPayload[] = [];
 
   constructor(
     public pushService: PushService
   ) {}
 
+  ngOnInit() {}
+
+  ionViewWillEnter() {}
 }
--- src/app/services/push.service.ts
+++ src/app/services/push.service.ts
@@ -1,18 +1,22 @@
-import { Injectable } from '@angular/core';
-import { OneSignal, OSNotification } from '@ionic-native/onesignal/ngx';
+import { Injectable, ApplicationRef } from '@angular/core';
+import { OneSignal, OSNotification, OSNotificationPayload } from '@ionic-native/onesignal/ngx';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PushService {
 
-  messages: any[] = [{
-    title: 'Titulo',
-    body: 'Cuerpo del mensaje',
-    date: new Date(),
-  }];
+  MESSAGES_KEY = 'messages';
+  messages: OSNotificationPayload[] = [];
 
-  constructor(private oneSignal: OneSignal) { }
+  constructor(
+    private oneSignal: OneSignal,
+    private storage: Storage,
+    private aplicationRef: ApplicationRef
+  ) {
+    this.loadMessages();
+  }
 
   initConfiguration() {
     this.oneSignal.startInit('50d39620-0cc7-4a70-bff8-0ca08954aae0', '289491499609');
@@ -25,18 +29,27 @@ export class PushService {
 
     this.oneSignal.handleNotificationOpened().subscribe((notification) => {
       // do something when a notification is opened
-      console.log('Notificación abierta', notification);
     });
 
     this.oneSignal.endInit();
   }
 
-  notificationReceived(notification: OSNotification) {
+  notificationReceived(notification: any) {
     const payload = notification.payload;
-    const existMessage = this.messages.find(m => m.notificationId === payload.notificationID);
+    const existMessage = this.messages.find(m => m.notificationID === payload.notificationID);
 
     if (!existMessage) {
       this.messages.unshift( payload );
+      this.aplicationRef.tick(); // Update view with new messages
+      this.saveMessages();
     }
+  }
+
+  saveMessages() {
+    this.storage.set( this.MESSAGES_KEY, this.messages );
+  }
+
+  async loadMessages() {
+    this.messages = await this.storage.get( this.MESSAGES_KEY ) || [];
   }
 }
